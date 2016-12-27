667146d6dda387f8f623b642372cd7e22d5ca3c1
Amol Patil <computer.warriors2020@gmail.com>
2016-12-28 02:44:55 +0530

    Index Page
    Contain the links of pages so we don't need to write code repeatedly.

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+
+  <head>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Water Distribution System</title>
+
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/css/my.css" rel="stylesheet">
+
+	<script src="bootstrap/jq/jquery.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+  <?php include("header.php");?>
+ <?php include("navbar.php");?>
+  <?php include("footer.php");?>
+ 
+  </body>
+</html>
