36329a70c1d82be7d920d9cc8fa058e7952ee31e
Amol Patil <computer.warriors2020@gmail.com>
2016-12-28 02:38:39 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Header</title>
+</head>
+
+<body>
+	<div class="container">
+		<div class="row">
+     	    <div class="col-sm-4">
+             <img src="images/pmclogo.jpeg" class="img" alt="Cinque Terre" width="204" height="136">
+             </div>
+      		<div class="col-sm-8"><br/><h1 align="center">Smart Water Distribution System</h1></div>
+      	</div>
+	</div>
+
+
+
+
+
+</body>
+</html>
