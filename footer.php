47ece2dc23a95bd1e13b60fbaad630536dfeb7d3
Amol Patil <computer.warriors2020@gmail.com>
2016-12-28 02:43:08 +0530

    Footer

---
+++ footer.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Footer</title>
+</head>
+
+<body>
+	
+	<footer id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+			&copy; 2017 <a href="#" title="Water Managament">Water Distibution Department</a>. All Rights Reserved.
+			</div>
+			
+			<div class="col-sm-6 pull-right">
+				<ul class="pull-right">
+					<li>Developed By : <a target="_blank" href="https://in.linkedin.com/in/amolpatilcan"> Amol Patil</a></li>
+				</ul>
+				<br>
+			</div>
+		</div>
+	</div>
+</footer> 
+
+
+</body>
+</html>
