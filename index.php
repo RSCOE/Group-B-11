0257eb208f05c3d43ae9a714e34c56f584eec006
Amol Patil <computer.warriors2020@gmail.com>
2017-01-16 19:32:58 +0530

    Index of all pages

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
   <head>
-
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Water Distribution System</title>
-
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="bootstrap/css/my.css" rel="stylesheet">
-
 	<script src="bootstrap/jq/jquery.min.js"></script>
 	<script src="bootstrap/js/bootstrap.min.js"></script>
   </head>
   <body>
-
   <?php include("header.php");?>
  <?php include("navbar.php");?>
+ <?php include("banner.php");?>
+ <?php include("body.php");?>
+  <?php include("complaint.php");?>
   <?php include("footer.php");?>
  
   </body>
