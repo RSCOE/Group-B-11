6c37eebc9268245e8755f704c642f8980b508cde
Amol Patil <computer.warriors2020@gmail.com>
2017-01-16 19:31:19 +0530

    Schedule Page

---
+++ body.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+<title></title>
+</head>
+
+<body>
+	<div class="container bg-warning">
+  <h2 align="center">Water Distribution Schedule</h2>
+  
+  <table class="table">
+    <thead>
+      <tr>
+      	<th>Sr.No.</th>
+        <th>Street</th>
+		<th>Day</th>
+        <th>Time</th>
+       
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="success">
+        <td>1</td>
+        <td>M.G Road</td>
+		<td>Monday</td>
+        <td>7:00am</td>
+        
+      </tr>
+      <tr class="danger">
+       <td>2</td>
+        <td>JW Road</td>
+		<td>Monday</td>
+        <td>8:00am</td>
+        
+      </tr>
+      <tr class="info">
+      	 <td>3</td>
+        <td>Vikas Nagar</td>
+        <td>Monday</td>
+        <td>2:00pm</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<br>
+
+</body>
+</html>
